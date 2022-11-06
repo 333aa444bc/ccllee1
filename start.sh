365693385ee9430c1959904cf4c84946d5d765bf
333aa444bc <97817183+333aa444bc@users.noreply.github.com>
2022-11-06 19:41:26 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=f5943cf4-43d2-4695-a65c-52db6b0e4271
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=github.com/wulabing/3DCEList/archive/master.zip
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
